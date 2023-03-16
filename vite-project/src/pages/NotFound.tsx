18d64870e5d8cddef4529366d9474b9f3807cd7c
artemmyzrov <a.myzrov@yandex.ru>
2023-03-16 22:18:20 +0400

    feat: React-Router version 6 , paths and componets

---
+++ vite-project/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../pages/NotFound.module.css';
+
+function NotFound() {
+  return (
+    <div className={styles.notFound}>
+      <p>404</p>
+      <h2>Sorry,pages Not Found ...</h2>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ vite-project/src/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/header';
+import styles from './home.module.css';
+
+const home = () => {
+  return (
+    <>
+      <div className={styles.home}>
+        <h2>Home</h2>
+      </div>
+    </>
+  );
+};
+
+export default home;
