e1c911f291000ff03a71c6df79e5fb24cc8f89e0
artemmyzrov <a.myzrov@yandex.ru>
2023-03-30 14:50:56 +0400

    refactor: wrote a form on hooks

--- vite-project/src/pages/form.tsx
+++ vite-project/src/pages/form.tsx
@@ -1,4 +1,5 @@
-import React, { Component, FormEvent } from 'react';
+import React from 'react';
+import { useState, useRef } from 'react';
 import FormCards from '../components/form/formCards';
 import styles from './form.module.css';
 import { Items } from 'components/product.model';
@@ -10,188 +11,168 @@ const isUppercase = (a: string) => {
   return /^\p{Lu}/u.test(a);
 };
 
-class Form extends Component {
-  state = {
-    inputName: '',
-    inputNameError: false,
-    inputDate: '',
-    inputDateError: false,
-    inputCountry: '',
-    inputCountryError: false,
-    inputYear: false,
-    inputNow: false,
-    inputImg: '',
+const Form: React.FC = () => {
+  const [inputName, setInputName] = useState<string>('');
+  const [inputNameError, setInputNameError] = useState<boolean>(false);
+  const [inputDate, setInputDate] = useState<string>('');
+  const [inputDateError, setInputDateError] = useState<boolean>(false);
+  const [inputCountry, setInputCountry] = useState<string>('');
+  const [inputCountryError, setInputCountryError] = useState<boolean>(false);
+  const [inputYear, setInputYear] = useState<boolean>(false);
+  const [inputNow, setInputNow] = useState<boolean>(false);
+  const [inputImg, setInputImg] = useState<string>('');
+
+  const inputNameRef = useRef<HTMLInputElement>(null);
+  const inputDateRef = useRef<HTMLInputElement>(null);
+  const inputCountryRef = useRef<HTMLSelectElement>(null);
+  const inputNowRef = useRef<HTMLInputElement>(null);
+  const inputYearRef = useRef<HTMLInputElement>(null);
+  const inputImgRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = () => {
+    setInputName(inputNameRef.current?.value || '');
+    setInputDate(inputDateRef.current?.value || '');
+    setInputCountry(inputCountryRef.current?.value || '');
+    setInputYear(inputYearRef.current?.checked || false);
+    setInputNow(inputNowRef.current?.checked || false);
+    setInputImg(inputImgRef.current?.value || '');
   };
 
-  inputNameRef = React.createRef<HTMLInputElement>();
-  inputDateRef = React.createRef<HTMLInputElement>();
-  inputCountryRef = React.createRef<HTMLSelectElement>();
-  inputNowRef = React.createRef<HTMLInputElement>();
-  inputYearRef = React.createRef<HTMLInputElement>();
-  inputImgRef = React.createRef<HTMLInputElement>();
-
-  handleChange = () => {
-    this.setState({
-      inputName: this.inputNameRef.current?.value,
-      inputDate: this.inputDateRef.current?.value,
-      inputCountry: this.inputCountryRef.current?.value,
-      inputYear: this.inputYearRef.current?.checked,
-      inputNow: this.inputNowRef.current?.checked,
-      inputImg: this.inputImgRef.current?.value,
-    });
-  };
-
-  handleSub = (e: FormEvent) => {
+  const handleSub = (e: React.FormEvent) => {
     e.preventDefault();
-    const { inputName, inputDate, inputCountry, inputYear, inputNow, inputImg } = this.state;
 
-    if (!isUppercase(this.state.inputName)) {
-      this.setState({ inputNameError: true });
+    if (!isUppercase(inputName)) {
+      setInputNameError(true);
       return;
     } else {
-      this.setState({ inputNameError: false });
+      setInputNameError(false);
     }
 
-    if (!this.state.inputDate) {
-      this.setState({ inputDateError: true });
+    if (!inputDate) {
+      setInputDateError(true);
       return;
     } else {
-      this.setState({ inputDateError: false });
+      setInputDateError(false);
     }
-    if (!this.state.inputCountry) {
-      this.setState({ inputCountryError: true });
+    if (!inputCountry) {
+      setInputCountryError(true);
       return;
     } else {
-      this.setState({ inputCountryError: false });
+      setInputCountryError(false);
     }
 
     itemsCard.push({
-      inputName: inputName,
-      inputDate: inputDate,
-      inputCountry: inputCountry,
-      inputYear: inputYear,
-      inputNow: inputNow,
-      inputImg: inputImg,
+      inputName,
+      inputDate,
+      inputCountry,
+      inputYear,
+      inputNow,
+      inputImg,
       id: id++,
     });
-    this.setState({
-      inputName: '',
-      inputNameError: false,
-      inputDate: '',
-      inputDateError: false,
-      inputCountry: '',
-      inputCountryError: false,
-      inputYear: false,
-      inputNow: false,
-      inputImg: '',
-    });
+    setInputName('');
+    setInputDate('');
+    setInputCountry('');
+    setInputYear(false);
+    setInputNow(false);
+    setInputImg('');
 
     window.alert(' Thanks we have added your details !');
     console.log(itemsCard);
   };
 
-  render() {
-    const { inputName, inputDate, inputCountry, inputYear, inputNow, inputImg } = this.state;
-
-    return (
-      <div className={styles.wrap}>
-        <div className={styles.formPage}>
-          <form onSubmit={this.handleSub}>
-            <label htmlFor="fullName">Full Name</label>
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.formPage}>
+        <form onSubmit={handleSub}>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            onChange={handleChange}
+            value={inputName}
+            type="text"
+            placeholder="Capitalized name please"
+            name="fullName"
+            id="fullName"
+            ref={inputNameRef}
+          />
+          {inputNameError && (
+            <label style={{ color: 'yellow' }}>*you didn&apos;t enter a name</label>
+          )}
+          <label htmlFor="DOB">Date of Birth</label>
+          <input
+            onChange={handleChange}
+            value={inputDate}
+            type="date"
+            name="DOB"
+            id="DOB"
+            ref={inputDateRef}
+          />
+          {inputDateError && (
+            <label style={{ color: 'yellow' }}>*you have not entered a date</label>
+          )}
+          <label htmlFor="country">Country</label>
+          <select
+            value={inputCountry}
+            onChange={handleChange}
+            id="country"
+            name="country"
+            ref={inputCountryRef}
+          >
+            <option value="">Choose a country</option>
+            <option value="US">United States of America</option>
+            <option value="UK">United Kingdom</option>
+            <option value="Canada">Canada</option>
+          </select>
+          {inputCountryError && (
+            <label style={{ color: 'yellow' }}>*you have not entered a country</label>
+          )}
+          <br />
+          <label htmlFor="ageVerification">Are you 18 years of age or older? </label>
+          <div className={styles.years}>
             <input
-              onChange={this.handleChange}
-              value={inputName}
-              type="text"
-              placeholder="Capitalized name please"
-              name="fullName"
-              id="fullName"
-              ref={this.inputNameRef}
+              onChange={handleChange}
+              type="checkbox"
+              name="ageVerification"
+              checked={inputYear}
+              ref={inputYearRef}
             />
-            {this.state.inputNameError && (
-              <label style={{ color: 'yellow' }}>*you didn&apos;t enter a name</label>
-            )}
-            <label htmlFor="DOB">Date of Birth</label>
+            Yes
+            <br />
+          </div>
+          <br />
+          <label htmlFor="now"> Is it now?: </label>
+          <div className={styles.now}>
             <input
-              onChange={this.handleChange}
-              value={inputDate}
-              type="date"
-              name="DOB"
-              id="DOB"
-              ref={this.inputDateRef}
+              onChange={handleChange}
+              type="radio"
+              name="now"
+              checked={inputNow}
+              value="yes"
+              ref={inputNowRef}
             />
-            {this.state.inputDateError && (
-              <label style={{ color: 'yellow' }}>*you have not entered a date</label>
-            )}
-            <label htmlFor="country">Country</label>
-            <select
-              value={inputCountry}
-              onChange={this.handleChange}
-              id="country"
-              name="country"
-              ref={this.inputCountryRef}
-            >
-              <option value="">Choose a country</option>
-              <option value="US">United States of America</option>
-              <option value="UK">United Kingdom</option>
-              <option value="Canada">Canada</option>
-            </select>
-            {this.state.inputCountryError && (
-              <label style={{ color: 'yellow' }}>*you have not entered a country</label>
-            )}
+            Yes
             <br />
-            <label htmlFor="ageVerification">Are you 18 years of age or older? </label>
-            <div className={styles.years}>
-              <input
-                onChange={this.handleChange}
-                type="checkbox"
-                name="ageVerification"
-                checked={inputYear}
-                ref={this.inputYearRef}
-              />
-              Yes
-              <br />
-            </div>
+            <input onChange={handleChange} type="radio" name="now" checked={!inputNow} value="no" />
+            No
+          </div>
+          <label htmlFor="file">Upload Image: </label>
+          <div className="addImg">
+            <input
+              onChange={handleChange}
+              value={inputImg}
+              type="file"
+              id="file"
+              ref={inputImgRef}
+            />
             <br />
-            <label htmlFor="now"> Is it now?: </label>
-            <div className={styles.now}>
-              <input
-                onChange={this.handleChange}
-                type="radio"
-                name="now"
-                checked={inputNow}
-                value="yes"
-                ref={this.inputNowRef}
-              />
-              Yes
-              <br />
-              <input
-                onChange={this.handleChange}
-                type="radio"
-                name="now"
-                checked={!inputNow}
-                value="no"
-              />
-              No
-            </div>
-            <label htmlFor="file">Upload Image: </label>
-            <div className="addImg">
-              <input
-                onChange={this.handleChange}
-                value={inputImg}
-                type="file"
-                id="file"
-                ref={this.inputImgRef}
-              />
-              <br />
-            </div>
-            <br />
-            <button className={styles.submit}>Submit</button>
-          </form>
-        </div>
-        <FormCards items={itemsCard} />
+          </div>
+          <br />
+          <button className={styles.submit}>Submit</button>
+        </form>
       </div>
-    );
-  }
-}
+      <FormCards items={itemsCard} />
+    </div>
+  );
+};
 
 export default Form;
